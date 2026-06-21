0c71d3caa3b9ea189972d292fa081b449d628071
yosama232333-dot <yosama232333@gmail.com>
2026-06-22 00:40:45 +0300

    Add files via upload

---
+++ @radix-ui_react-slot.js
@@ -0,0 +1,15 @@
+import {
+  Slot,
+  Slottable,
+  createSlot,
+  createSlottable
+} from "./chunk-R3RT3XVX.js";
+import "./chunk-42LCURWG.js";
+import "./chunk-G3PMV62Z.js";
+export {
+  Slot as Root,
+  Slot,
+  Slottable,
+  createSlot,
+  createSlottable
+};
